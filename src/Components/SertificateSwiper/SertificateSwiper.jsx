158bc3522931a8b6ba5b1c1d0910de894bbc972a
wolamey <salamenikava77@gmail.com>
2024-09-23 18:14:20 +0300

    fix

--- src/Components/SertificateSwiper/SertificateSwiper.jsx
+++ src/Components/SertificateSwiper/SertificateSwiper.jsx
@@ -1,14 +1,14 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { Pagination, EffectCoverflow, Navigation } from 'swiper/modules';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
+import cross from '../../assets/cross.png';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-coverflow';
-import Slider from '../Slider/Slider';
-
 import './SertificateSwiper.scss';
 
 import sertificate1 from '../../assets/sertificate1.png';
@@ -18,48 +18,93 @@ import sertificate4 from '../../assets/sertificate4.png';
 import sertificate5 from '../../assets/sertificate5.png';
 import sertificate6 from '../../assets/sertificate6.png';
 
+// Установить стили для react-modal
+Modal.setAppElement('#root'); // Это важно для лучшего взаимодействия с экранными читалками и для устранения предупреждений
+
 export default function SertificateSwiper() {
-  const images = [sertificate1, sertificate2];
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const images = [
+    sertificate1, sertificate2, sertificate3,
+    sertificate4, sertificate5, sertificate6
+  ];
+
+  const openModal = (index) => {
+    setCurrentSlide(index);
+    setIsOpen(true);
+    document.querySelector('body').style.overflowY = 'hidden';
+    
+  };
+
+  const closeModal = () =>{ setIsOpen(false);
+    document.querySelector('body').style.overflowY = 'auto';
+
+  };
+
   return (
-    <Swiper
-      effect={'slide'}
-      grabCursor={true}
-      centeredSlides={true}
-      slidesPerView={window.innerWidth < 1024 ? 1 : 3}
-      spaceBetween={30}
-      loop={true}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 0,
-        depth: 200,
-        modifier: 0,
-        slideShadows: true,
-      }}
-      navigation={true}
-      pagination={{
-        dynamicBullets: true,
-      }}
-      modules={[EffectCoverflow, Pagination, Navigation]}
-      className="mySwiper"
-    >
-      <SwiperSlide className="init_container">
-        <img src={sertificate6} alt="" />
-      </SwiperSlide>
-      <SwiperSlide className="init_container">
-        <img src={sertificate2} alt="" />
-      </SwiperSlide>
-      <SwiperSlide className="init_container">
-        <img src={sertificate3} alt="" />
-      </SwiperSlide>
-      <SwiperSlide className="init_container">
-        <img src={sertificate4} alt="" />
-      </SwiperSlide>
-      <SwiperSlide className="init_container">
-        <img src={sertificate1} alt="" />
-      </SwiperSlide>
-      <SwiperSlide className="init_container">
-        <img src={sertificate5} alt="" />
-      </SwiperSlide>
-    </Swiper>
+    <>
+      <Swiper
+        effect={'slide'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={window.innerWidth < 1024 ? 1 : 3}
+        spaceBetween={30}
+        loop={true}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 200,
+          modifier: 0,
+          slideShadows: true,
+        }}
+        navigation={true}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide
+            key={index}
+            className="init_container"
+            onClick={() => openModal(index)}  // Открываем модалку при клике
+          >
+            <img src={image} alt={`sertificate${index + 1}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Модальное окно с полноэкранным слайдером */}
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="Sertificate Fullscreen"
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <button onClick={closeModal} className="close-button"><img src={cross} alt="" /></button>
+        <Swiper
+          effect={'slide'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          initialSlide={currentSlide} 
+          navigation={true}
+       
+          modules={[EffectCoverflow, Navigation]}
+          className="fullscreenSwiper"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index} className="fullscreen-slide">
+              <img src={image} alt={`fullscreen sertificate${index + 1}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Modal>
+    </>
   );
 }
